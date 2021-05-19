b2a18a35db23e8b5b90eb420504a6a33920ee9eb
Sristi27 <sristi2705@gmail.com>
2021-05-19 16:29:30 +0530

    rectified errors

--- client/src/components/newUser/newUser.jsx
+++ client/src/components/newUser/newUser.jsx
@@ -6,16 +6,16 @@ import signupIcon from './../../images/signupIcon.png'
 const newUser = () => {
     return (
         <div>
-            <div class="user-container">
+            <div className="user-container">
               
-              <p className="user-content">
+              <div className="user-content">
                 <h3 className="title">Welcome to Speak UP 
                 <span className="icon" style={{margin:'0 10px'}}>
                     <img src={signupIcon} width="55px" 
                 height="50px"/></span>
                 for women!</h3>
 
-<p className="user-text">As women, we believe your account of your experience 
+<div className="user-text">As women, we believe your account of your experience 
 and how you overcame issues can be of help to some other 
 woman going through the same.
 
@@ -34,19 +34,19 @@ about their issues without being judged or commented on by other people.
 
 
 <br/>
- <p className="caption">Go ahead and let out your thoughts!
-     </p>
+ <div className="caption">Go ahead and let out your thoughts!
+     </div>
 
- </p>
+ </div>
 
-<p className="title-btm">Love and Best wishes,
+<div className="title-btm">Love and Best wishes,
 Speak up Women Team<br/>
 <Link to="/signin"><button className="btn btn-primary user-login">
-    Join Us</button></Link></p>
-    </p>
+    Join Us</button></Link></div>
+    </div>
 <svg xmlns="http://www.w3.org/2000/svg"  className="user-svg"
 viewBox="0 0 1440 280">
-  <path fill="#BB377D" fill-opacity="1" 
+  <path fill="#BB377D" fillOpacity="1" 
   d="M0,224L80,192C160,160,320,96,480,112C640,128,800,224,960,229.3C1120,235,1280,149,1360,106.7L1440,64L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"></path>
 </svg>
 </div>
